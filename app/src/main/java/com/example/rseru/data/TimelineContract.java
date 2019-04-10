a26cdd74bcbf55227108f4a40fbdac481d73044a
GadzhievPavel <gadzhievpasha@gmail.com>
2019-04-10 18:08:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rseru/data/TimelineContract.java
@@ -0,0 +1,19 @@
+package com.example.rseru.data;
+
+import android.provider.BaseColumns;
+
+public final class TimelineContract implements BaseColumns {
+    public final static String TABEL_NAME="timeline";
+    public final static String COLUMN_GROUP="group";
+    public final static String COLUMN_TYPE_LESSON="typeLesson";
+    public final static String COLUMN_SUBJECT="subject";
+    public final static String COLUMN_TEACHER="teacher";
+    public final static String COLUMN_START_TIME="startTime";
+    public final static String COLUMN_FINISH_TIME="finishTime";
+    public final static String COLUMN_NOMINATOR="nominator";
+    public final static String COLUMN_DENOMINATOR="denominator";
+    public final static String COLUMN_DAYWEEK="dayweek";
+    public final static String COLUMN_MOUNTH="mounth";
+    public final static String COLUMN_AUDITORY="auditory";
+
+}
---
+++ app/src/main/java/com/example/rseru/data/TimelineDBHelper.java
@@ -0,0 +1,16 @@
+package com.example.rseru.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TimelineDBHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
